41f73b7a5956edb78e1eb7cf51b9f218ce00dbc0
kkattt51 <10tany2010.10@gmail.com>
2021-10-29 14:07:21 +0300

    second commit: created models

---
+++ src/main/java/tech/ik/employeemanager/model/Employee.java
@@ -0,0 +1,64 @@
+package tech.ik.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTile;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public Employee() {}
+
+    public Employee(String name, String email, String jobTile, String phone, String imageUrl, String employeeCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTile = jobTile;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getJobTile() {return jobTile;}
+    public void setJobTile(String jobTile) {this.jobTile = jobTile;}
+
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getImageUrl() {return imageUrl;}
+    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+
+    public String getEmployeeCode() {return employeeCode;}
+    public void setEmployeeCode(String employeeCode) {this.employeeCode = employeeCode;}
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTile + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
