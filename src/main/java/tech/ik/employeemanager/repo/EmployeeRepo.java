1d26348bdf084934378ec9598820d0f9c374cd7b
kkattt51 <10tany2010.10@gmail.com>
2021-10-30 20:50:12 +0300

    fourth commit: created methods

--- src/main/java/tech/ik/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/tech/ik/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package tech.ik.employeemanager.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.ik.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/tech/ik/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/ik/employeemanager/service/EmployeeService.java
@@ -2,9 +2,11 @@ package tech.ik.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.ik.employeemanager.exception.UserNotFoundException;
 import tech.ik.employeemanager.model.Employee;
 import tech.ik.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id" + id + "was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
     }
 }
